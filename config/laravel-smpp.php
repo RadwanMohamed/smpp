a9ada2501a00b7320b2f851ae2b3763c2d6482c5
RadwanMohamed123 <radwan.mohamed@jobzella.com>
2023-05-09 21:57:07 +0300

    smpp

--- config/laravel-smpp.php
+++ config/laravel-smpp.php
@@ -41,7 +41,7 @@ return [
         'BRN' => [
             'host' => '196.204.229.70',
             'port' => 8899,
-            'timeout' => 30000,
+            'timeout' => 10000,
             'login' => 'BrandSMPP',
             'password' => 'Bra4838'
         ]
